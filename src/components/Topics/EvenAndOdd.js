4adbc70bde9396e4f6b68d74e5367eee3ce8bf54
Nathaniel Shirts <nshirts7@gmail.com>
2021-03-04 18:32:58 -0700

    added structure to all the comps

---
+++ src/components/Topics/EvenAndOdd.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EvenAndOdd extends Component {
+  render() {
+    return (
+      <div>
+        <p>Even and Odd</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/Topics/FilterObject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class FilterObject extends Component {
+  render() {
+    return (
+      <div>
+        <p>Filter Object</p>
+      </div>
+    );
+  }
+}
